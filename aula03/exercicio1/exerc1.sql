83a59f601a7253050ff6b7a6a6c94b9fd045dacf
Beatriz Woos Buffon <116319748+beatrizbuffon21@users.noreply.github.com>
2025-08-19 09:08:19 -0300

    Update exerc1.sql

--- aula03/exercicio1/exerc1.sql
+++ aula03/exercicio1/exerc1.sql
@@ -1,55 +1,34 @@
--- exerc2 ---------------------------------------------------------------------------------------------------------
+-- exerc1 ---------------------------------------------------------------------------------------------------------
 
 -- criar tabelas:
-create table automovel(placa varchar(30) primary key,
-						marca varchar(30),
-						modelo varchar(30),
-						cor varchar(30));
+create table empregado(num_emp integer primary key,
+						nome_emp varchar(30),
+						salario decimal(10,2),
+						num_dept decimal(10,2));
 
-create table propriedade (cod_propriedade integer primary key,
-							placa varchar(30),
-							cod_pessoa varchar(30));
+create table departamento (num_dept integer primary key,
+							nome varchar(30),
+							ramal decimal(10,2));
 
-create table pessoa (cod_pessoa varchar(30) primary key,
-						nome varchar(30),
-						endereco varchar(30),
-						sexo varchar(30));
-
--- inserir dados na tabela:
-
-INSERT INTO automovel (placa, marca, modelo, cor)
-VALUES
-    ('A1', 'Honda', 'Fit', 'Prata'),
-    ('A2', 'Chevrolet', 'Astra', 'Branca');
-
-INSERT INTO propriedade (cod_propriedade, placa, cod_pessoa)
-VALUES
-    (1, 'A1', 'P1'),
-    (2, 'A2', 'P2');
-
-INSERT INTO pessoa (cod_pessoa, nome, endereco, sexo)
-VALUES
-    ('P1', 'A', 'E1', 'M'),
-    ('P2', 'B', 'E2', 'F');
-	
 -- criar gatilho para a tabela auditoria:
 create table func_auditoria (operacao varchar(1),
 								usuario varchar(30),
 								data timestamp,
-								cod_propriedade decimal(10,2));
+								num_dept decimal(10,2));
+
 
 -- criar função que tenha sequência de comando:
 
 create function processo_audit_func() returns trigger as $$
 begin
 	if (tg_op = 'DELETE') then
-		insert into func_auditoria values ('E', user, now(), old.cod_propriedade);
+		insert into func_auditoria values ('E', user, now(), old.num_dept);
 		return old;
 	elseif (tg_op = 'UPDATE') then
-		insert into func_auditoria values ('A', user, now(), old.cod_propriedade);
+		insert into func_auditoria values ('A', user, now(), old.num_dept);
 		return new;
 	elseif (tg_op = 'INSERT') then
-		insert into func_auditoria values ('I', user, now(), new.cod_propriedade);
+		insert into func_auditoria values ('I', user, now(), new.num_dept);
 		return new;
 	end if;
 	return null;
@@ -60,17 +39,30 @@ $$ language plpgsql;
 -- criar trigger funcionario_audit:
 
 create trigger funcionario_audit
-after insert or update or delete on propriedade
+after insert or update or delete on empregado
 for each row execute procedure processo_audit_func();
 
+-- inserir dados na tabela:
 
--- cadastrar novo automovel na tabela automovel:
-INSERT INTO automovel (placa, marca, modelo, cor)
+INSERT INTO empregado (num_emp, nome_emp, salario, num_dept)
 VALUES
-    ('A3', 'Fiat', 'Palio', 'Preto');
-	
+    (32, 'J Silva', 3800, 21),
+    (74, 'M Reis', 4000, 25),
+	(89, 'C Melo', 5200, 28),
+    (92, 'R Silva', 4800, 25),
+    (112, 'R Pinto', 3900, 21),
+    (121, 'V Simão', 1905, 28);
+
+
+INSERT INTO departamento (num_dept, nome, ramal)
+VALUES
+    (21, 'Pessoal', 142),
+    (25, 'Financeiro', 143),
+	(28, 'Técnico', 144);
+
+
 -------- TESTE --------
 -- inserir dados para testar o gatilho:
-insert into propriedade(cod_propriedade, placa, cod_pessoa)
-VALUES (3, 'A3', 'P2');
+insert into empregado(num_emp, nome_emp, salario, num_dept)
+VALUES (131, 'L Silveira', 3500, 28);
 
