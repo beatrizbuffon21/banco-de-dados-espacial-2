c7eed1a5f03b079e9cf498bccc935cba46451e9b
Beatriz Woos Buffon <116319748+beatrizbuffon21@users.noreply.github.com>
2025-08-19 09:34:17 -0300

    Update exercicio01-pratica.sql

--- aula02/exercicio/exercicio01-pratica.sql
+++ aula02/exercicio/exercicio01-pratica.sql
@@ -1,16 +1,16 @@
 -- EXERCÍCIO 1 PRÁTICA ---------------------------------------------------------------------------------------------------------
 -- criando as tabelas:
 
-create table marin(mid integer primary key,
+CREATE table marin(mid integer primary key,
 					mnome varchar(30),
 					status integer,
 					idade numeric(10,2));
 
-create table barcos(bid integer primary key,
+CREATE table barcos(bid integer primary key,
 						bnome varchar(30),
 						cor varchar(30));
 
-create table reserva(rid integer primary key,
+CREATE table reserva(rid integer primary key,
 						mid integer,
 						bid integer,
 						dia date);
@@ -19,14 +19,14 @@ create table reserva(rid integer primary key,
 -- criando um gatilho para a tabela auditoria(operacao, usuário, data e hora, codigo do
 -- marinheiro) para ver qual usuário fez a reserva e em que data e horário
 
-create table func_auditoria (operacao varchar(1),
+CREATE table func_auditoria (operacao varchar(1),
 								usuario varchar(30),
 								data timestamp,
 								mid integer);
 
 -- criar função que tenha sequência de comando:
 
-create function processo_audit_func() returns trigger as $$
+CREATE function processo_audit_func() returns trigger as $$
 begin
 	if (tg_op = 'DELETE') then
 		insert into func_auditoria values ('E', user, now(), old.mid);
@@ -44,7 +44,7 @@ $$ language plpgsql;
 
 -- criando um gatilho que antes da inserção na tabela MARIN verifica se o nome do marinheiro é nulo.
 
-create function verifica() returns trigger as $$
+CREATE function verifica() returns trigger as $$
 begin 
 	if new.mnome is null then 
 		raise exception 'O nome do marinheiro não pode ser nulo';
@@ -56,11 +56,11 @@ begin
 
 -- criar trigger funcionario_audit:
 
-create trigger funcionario_audit
-after insert or update or delete on reserva
+CREATE trigger funcionario_audit
+after INSERT or UPDATE or DELETE on reserva
 for each row execute procedure processo_audit_func();
 
-create trigger novo_func before insert or update on marin
+CREATE trigger novo_func before INSERT or UPDATE on marin
 for each row execute procedure verifica();
 
 ------ OBS: importar os arquivos de cada tabela clicando em cima da tabela específica
@@ -69,10 +69,12 @@ for each row execute procedure verifica();
 
 -------- TESTES --------
 -- teste do nome ser nulo
-insert into marin(mid, status, idade) values(3, 0, 40);
+INSERT into marin(mid, status, idade)
+VALUES(3, 0, 40);
 
 -- atualização do bid em reserva:
-update reserva set bid = 300;
+UPDATE reserva SET bid = 300;
+
 
 
 
