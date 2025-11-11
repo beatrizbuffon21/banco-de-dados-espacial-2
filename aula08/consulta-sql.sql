b04333e457854cc28343a75022a52480ba41f195
Beatriz Woos Buffon <116319748+beatrizbuffon21@users.noreply.github.com>
2025-11-11 11:18:40 -0300

    Add files via upload

---
+++ aula08/consulta-sql.sql
@@ -0,0 +1,8 @@
+select mercados.geom from bairro, mercados
+where st_contains(bairro.geom, mercados.geom)
+and bairro.nome = 'Camobi'
+
+
+select mercados.cod, mercados.geom from bairro, mercados
+where st_dwithin(st_transform(bairro.geom, 31982), st_transform(mercados.geom, 31982), 5000)
+and bairro.nome = 'Camobi'
